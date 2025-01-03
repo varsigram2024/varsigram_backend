5c3d682ffe8063ff41ab609c8494016dd6206042
Certifieddonnie <mcdonaldsamure91@gmail.com>
2025-01-03 15:54:37 +0100

    [test]: completed

--- varsigram/chat/views.py
+++ varsigram/chat/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics, permissions
 from .models import Message, User
 from rest_framework.exceptions import ValidationError
 from .serializer import MessageSerializer
+from django.db.models import Q
 
 class MessageListCreateView(generics.ListCreateAPIView):
     serializer_class = MessageSerializer
@@ -10,7 +11,16 @@ class MessageListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        return Message.objects.filter(sender=user) | Message.objects.filter(receiver=user)
+        other_user_id = self.request.query_params.get('other_user')
+        if other_user_id:
+            try:
+                other_user = User.objects.get(pk=other_user_id)
+                return Message.objects.filter(
+                    Q(sender=user, receiver=other_user) | Q(sender=other_user, receiver=user)
+                ).order_by('date')
+            except User.DoesNotExist:
+                return Message.objects.none()
+        return Message.objects.filter(Q(sender=user) | Q(receiver=user)).order_by('date')
 
     def perform_create(self, serializer):
         receiver_id = self.request.data.get('receiver')
