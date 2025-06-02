8d5ec614d63b79d521f4d1e65a89372d43d71f65
Certifieddonnie <mcdonaldsamure91@gmail.com>
2025-06-02 16:07:32 +0100

    [feat]: postgres

--- varsigram/varsigram/settings.py
+++ varsigram/varsigram/settings.py
@@ -100,13 +100,22 @@ WSGI_APPLICATION = 'varsigram.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME", "varsigram"),
+        'USER': os.environ.get("DB_USER", "postgres"),
+        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
+        'HOST': os.environ.get("DB_HOST", "localhost"),
+        'PORT': os.environ.get("DB_PORT", "5432"),
+        'ATOMIC_REQUESTS': True,
+        'OPTIONS': {
+            'options': '-c search_path=django,public'
+        }
+
     }
 }
+
 
 # Rest Framework Settings
 REST_FRAMEWORK = {
