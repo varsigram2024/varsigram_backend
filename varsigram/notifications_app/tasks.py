43c54cbf55881f86531ac1276ac5f84727be8302
Certifieddonnie <mcdonaldsamure91@gmail.com>
2025-10-28 21:43:14 +0100

    [bug]: notification

--- varsigram/notifications_app/tasks.py
+++ varsigram/notifications_app/tasks.py
@@ -3,12 +3,22 @@ from .utils import send_push_notification
 from users.models import User
 
 @shared_task
-def notify_all_users_new_post(author_id, author_name, post_content, post_id, author_profile_pic_url):
+def notify_all_users_new_post(author_id, author_name, post_content, post_id, author_profile_pic_url=None, **kwargs):
+    """Notify all users about a new post.
+
+    The task accepts an optional `author_profile_pic_url` and extra kwargs to
+    remain compatible with previously queued tasks that may include different
+    argument sets.
+    """
     users = User.objects.exclude(id=author_id)
     for user in users:
+        data_payload = {"type": "new_post", "post_id": post_id}
+        if author_profile_pic_url:
+            data_payload["author_profile_pic_url"] = author_profile_pic_url
+
         send_push_notification(
             user=user,
             title="New Post",
             body=f"{author_name} just posted: {post_content[:50]}...",
-            data={"type": "new_post", "post_id": post_id, "author_profile_pic_url": author_profile_pic_url}
+            data=data_payload
         )
